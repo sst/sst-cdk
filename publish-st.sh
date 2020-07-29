54fac42126f42f87ad6462fd85afcf2a836e61e8
Frank <wangfanjie@gmail.com>
2020-07-29 17:27:33 -0400

    Add publish script

--- publish-st.sh
+++ publish-st.sh
@@ -15,7 +15,7 @@ git tag v$version-st-$revision && git push --tags
 scripts/align-version.sh
 cd packages/aws-cdk
 sed -i '' "s/\"name\": \"aws-cdk\"/\"name\": \"@serverless-stack\/aws-cdk\"/g" package.json
-sed -i '' "s/\"version\": \"0.0.0\"/\"version\": \"$version-st-$revision\"/g" package.json
+sed -i '' "s/\"version\": \"$version\"/\"version\": \"$version-st-$revision\"/g" package.json
 npm publish --access public
 cd ../..
 git reset --hard
